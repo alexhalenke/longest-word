0b490de2a22de9290bca12df39c21b7319f1205e
Alexander Halenke <ahalenke@hotmail.com>
2023-10-25 18:42:08 +0200

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,24 @@
+import string
+import random
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = []
+        for i in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        if not word:
+            return False
+        letters = self.grid.copy()
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
---
+++ tests/test_game.py
@@ -0,0 +1,39 @@
+from longest_word.game import Game
+import string
+
+class TestGame:
+    def test_game_initialization(self):
+            # setup
+            new_game = Game()
+
+            # exercise
+            grid = new_game.grid
+
+            # verify
+            assert type(grid) == list
+            assert len(grid) == 9
+            for letter in grid:
+                assert letter in string.ascii_uppercase
+
+    def test_game_validity(self):
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'EUREKA'
+        new_game.grid = list(test_grid)
+        assert new_game.is_valid(test_word) is True
+        assert new_game.grid == list(test_grid)
+
+    def test_empty_word_is_invalid(self):
+        new_game = Game()
+        assert new_game.is_valid('') is False
+
+    def test_is_invalid(self):
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'SANDWICH'
+        # exerice
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is False
+        # teardown
+        assert new_game.grid == list(test_grid)
